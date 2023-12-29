67554b9c66404143719c123a1491acf890449094
mirno <mirno_hoogendonk@hotmail.com>
2023-12-30 00:48:53 +0100

    Commit first functional two player dartCounter with interfacing

--- cmd/dartcounter/main.go
+++ cmd/dartcounter/main.go
@@ -1,34 +1,73 @@
 package main
 
 import (
-    "dartCounter/pkg/game"
-    "fmt"
     "bufio"
+    "fmt"
     "os"
     "strconv"
     "strings"
+    "dartCounter/pkg/game"
+    "dartCounter/pkg/player"
+    "dartCounter/pkg/scoring"
 )
+
+// Optional put this in a utility package
+func isValidTurnScore(score int) bool {
+    // Scores that are not possible in a single turn
+    impossibleScores := map[int]bool{
+        159: true, 162: true, 163: true, 166: true, 169: true,
+        172: true, 173: true, 175: true, 176: true, 178: true, 179: true,
+    }
+
+    // Check if the score is in the range and not in the list of impossible scores
+    return score >= 0 && score <= 180 && !impossibleScores[score]
+}
 
 func main() {
     reader := bufio.NewReader(os.Stdin)
-    g := game.NewGame(501, 3) // Example: Start a game with 501 score and 3 target legs
+    fmt.Print("Enter name for Player 1: ")
+    name1, _ := reader.ReadString('\n')
+    name1 = strings.TrimSpace(name1)
 
+    fmt.Print("Enter name for Player 2: ")
+    name2, _ := reader.ReadString('\n')
+    name2 = strings.TrimSpace(name2)
+
+    p1 := player.NewPlayer(name1, 501)
+    p2 := player.NewPlayer(name2, 501)
+    scoringSystem := &scoring.StandardScoring{}
+    g := game.NewGame(p1, p2, scoringSystem)
+
+    currentPlayer := p1
     for {
+        fmt.Printf("Current player: %s (Score: %d)\n", currentPlayer.Name, currentPlayer.Score)
         fmt.Print("Enter score: ")
         input, _ := reader.ReadString('\n')
-        input = strings.TrimSpace(input)
-        score, err := strconv.Atoi(input)
-        if err != nil {
-            fmt.Println("Invalid input. Please enter a number.")
+        score, err := strconv.Atoi(strings.TrimSpace(input))
+        if err != nil || !isValidTurnScore(score) || score > currentPlayer.Score {
+            fmt.Println("Invalid total score for the turn. Please enter a valid score.")
             continue
         }
 
-        g.UpdateScore(score)
-        fmt.Printf("Current Score: %d, Legs Won: %d\n", g.CurrentScore, g.LegsWon)
+        currentPlayer.AddThrow(score)
+        g.UpdateScore(currentPlayer, score)
+        fmt.Printf("%s's Score: %d\n", currentPlayer.Name, currentPlayer.Score)
 
-        if g.LegsWon == g.TargetLegs {
-            fmt.Println("Game over!")
+        if g.CheckWinCondition() {
+            fmt.Printf("Player %s wins!\n", currentPlayer.Name)
             break
         }
+
+        // Switching the current player
+        if currentPlayer == p1 {
+            currentPlayer = p2
+        } else {
+            currentPlayer = p1
+        }
     }
+
+    // Display the scores thrown by each player
+    fmt.Println("Game Over! Final Throws:")
+    fmt.Printf("%s: %v\n", p1.Name, p1.Throws)
+    fmt.Printf("%s: %v\n", p2.Name, p2.Throws)
 }
---
+++ pkg/scoring/scoring.go
@@ -0,0 +1,8 @@
+package scoring
+
+import "dartCounter/pkg/player"
+
+type ScoringSystem interface {
+    UpdateScore(p *player.Player, score int)
+    CheckWinCondition(p1, p2 *player.Player) bool
+}
